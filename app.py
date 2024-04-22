1fa31eab5f940d55c4545910324bfebd397c9cc8
AllissonChervenski <allissonchervenski5@gmail.com>
2024-04-22 17:15:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def scrape():
 
 # Iniciar o servidor Flask
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Configurar para rodar no localhost
+    app.run(debug=True, host='0.0.0.0', port=8000)  # Configurar para rodar no localhost
